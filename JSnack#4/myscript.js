a59b278006e03edc21fbad0bb9106803192e3fd8
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-06 15:33:38 +0200

    initialized html + js

---
+++ JSnack#4/myscript.js
@@ -0,0 +1,7 @@
+/* In un array sono contenuti i nomi degli invitati alla festa del
+grande Gatsby, chiedi all’utente il suo nome e comunicagli se
+può partecipare o no alla festa. */
+
+
+// Dichiarazione delle variabili
+console.log("Ciao a tutti!");
