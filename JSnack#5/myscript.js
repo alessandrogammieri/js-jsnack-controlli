62f91f4afc90c139335c4d0af0931241d0bf8a8c
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-06 19:41:27 +0200

    end odd array

--- JSnack#5/myscript.js
+++ JSnack#5/myscript.js
@@ -3,4 +3,20 @@ un numero, se è dispari inseriscilo nell’array */
 
 
 // Dichiarazione delle variabili
-console.log("Ciao Mondo!");
+var dispari, n, sms = "L'array è vuoto";
+
+// Array vuoto
+dispari = [];
+
+// Richiesta all'utente di inserire un numero per 6 volte
+for (var i = 0; i < 6; i++) {
+  n = parseInt(prompt("Inserisci il tuo numero"));
+  // Se il numero è dispari inserirlo nell'array
+  if ((n % 2) != 0) {
+    dispari.push(n);
+    sms = "I numeri dispari nell'array sono: " + dispari;
+  }
+}
+
+// Comunicare all'utente quali numeri sono nell'array o se l'array è vuoto
+document.getElementById('mio_id').innerHTML = sms;
