a8402f89c0b94d207a740df7482b7d19a0926b80
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-06 16:51:43 +0200

    end check list

--- JSnack#4/myscript.js
+++ JSnack#4/myscript.js
@@ -4,4 +4,20 @@ può partecipare o no alla festa. */
 
 
 // Dichiarazione delle variabili
-console.log("Ciao a tutti!");
+var list, nome, elenco;
+
+// Elenco dei nomi invitati alla festa del grande Gatsby
+list = ["Luca", "Marco", "Nino", "Sergio"];
+
+// L'utente inserisce il suo nome per verificare
+nome = prompt("Inserisci qui il tuo nome");
+
+// Controlliamo se l'utente è presente
+elenco = list.includes(nome);
+
+// Comunichiamo all'utente se è invitato o meno
+if (elenco == true) {
+  document.getElementById('mio_id').innerHTML = nome + " sei invitato alla festa!";
+} else {
+  document.getElementById('mio_id').innerHTML = nome + " mi dispiace non sei invitato!";
+}
